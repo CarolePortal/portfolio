a20b327ce6ee48e1c759ed41912b3b1f0c52fd6d
cauxNicolas <nicaux95@gmail.com>
2023-03-09 11:16:14 +0100

    menu background active

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,11 @@
-const a = "test";
-console.log("🚀 ~ file: script.js:2 ~ a", a);
+// menu background active
+
+let menus = document.getElementById("Link");
+let navBtns = menus.getElementsByClassName("navBtn");
+for (let i = 0; i < navBtns.length; i++) {
+	navBtns[i].addEventListener("click", function () {
+		let a = document.getElementsByClassName("navActive");
+		a[0].className = a[0].className.replace(" navActive", "");
+		this.className += " navActive";
+	});
+}
